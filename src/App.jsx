be3694a5fc28aa99553680db1c6595b952cc980d
AntonioMBAir <67365572+EnFuegoV3@users.noreply.github.com>
2023-07-02 17:58:27 -1000

    added dice component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
+import './index.css'
+import { Die } from './components/Die'
 
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <main>
+      <div className='die-container'>
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/Die.jsx
@@ -0,0 +1,11 @@
+
+
+
+export function Die(props){
+
+    return (
+        <div className="die">
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    )
+}
